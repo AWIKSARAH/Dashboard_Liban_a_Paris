d0c4fa0b9921b5eba89dc5809e5950a7600ac547
Elalitareq <elalitareq@gmail.com>
2023-04-28 12:04:11 +0300

    fixed sidebar z-index to 999 so it goes over everything exept the close button

--- my-app/src/components/pageHeader/index.jsx
+++ my-app/src/components/pageHeader/index.jsx
@@ -1,6 +1,6 @@
 import "./pageHeader.css"
 function PageHeader(props) {
-    return ( <div className="page--header_container"><h1>{props.label}</h1></div> );
+    return ( <div className="page--header_container"><h2>{props.label}</h2></div> );
 }
 
 export default PageHeader;
